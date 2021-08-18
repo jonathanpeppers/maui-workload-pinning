bd6ca47a742b3aae6f940b20f4b1c1ab42c2fbfa
Jonathan Peppers <jonathan.peppers@microsoft.com>
2021-08-18 13:16:37 -0500

    dotnet new maui
    Then I deleted WinUI stuff

---
+++ maui-workload-pinning/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+using Microsoft.Maui;
+
+namespace maui_workload_pinning
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate<Startup>
+	{
+	}
+}
